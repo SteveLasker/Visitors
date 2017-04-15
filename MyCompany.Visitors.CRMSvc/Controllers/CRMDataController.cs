123ba4dce3461fe25833ace3b0cc91de7a20e080
Steve Lasker <StevenLasker@Hotmail.com>
2017-04-14 17:24:30 -0700

    Modernized Snap for 15.3 Pre-Preview

---
+++ MyCompany.Visitors.CRMSvc/Controllers/CRMDataController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompany.Visitors.CRMSvc.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CRMData")]
+    public class CRMDataController : Controller
+    {
+        // GET: api/CRMData
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {};
+        }
+
+        // GET: api/CRMData/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<CRMData> GetAsync(int id)
+        {
+
+            CRMData returndata = await GetCRMDataAsync(id);
+
+            return returndata;
+        }
+
+        // POST: api/CRMData
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/CRMData/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private async static Task<CRMData> GetCRMDataAsync(int id)
+        {
+            Random leads = new Random();
+
+            CRMData returndata = new CRMData()
+            {
+                VisitorId = id,
+                CRMAccountManager = "Cesar de_la_Torre",
+                CRMLeads = leads.Next(0, 5)
+            };
+
+            return returndata;
+        }
+
+        public class CRMData
+        {
+            public int VisitorId { get; set; }
+            public int CRMLeads { get; set; }
+            public string CRMAccountManager { get; set; }
+        }
+    }
+
+}
+
---
+++ MyCompany.Visitors.Web/Controllers/CRMController.cs
@@ -0,0 +1,45 @@
+﻿using MyCompany.Visitors.Model;
+using MyCompany.Visitors.Web.Infraestructure.Security;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace MyCompany.Visitors.Web.Controllers
+{
+    /// <summary>
+    /// CRMController integrates the CRMIntegrationService
+    /// </summary>
+    [RoutePrefix("api/crm")]
+    [MyCompanyAuthorization]
+    public class CRMController : ApiController
+    {
+        /// <summary>
+        /// Get CRMData from CRMIntegrationService
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/crm/5
+        [WebApiOutputCacheAttribute()]
+        [Route("{id:int:min(1)}")]
+        [Route("~/noauth/api/crm/{id:int:min(1)}")]
+        public async Task<CRMData> Get(int id)
+        {
+            HttpClient client = new HttpClient();
+            string hostname = "mycompany.visitors.crmsvc";
+
+            Uri uri = new Uri($"http://{hostname}/api/crmdata/{id}");
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            HttpContent content = response.Content;
+            CRMData returndata = await content.ReadAsAsync<CRMData>();
+
+            return returndata;
+        }
+    }
+}
