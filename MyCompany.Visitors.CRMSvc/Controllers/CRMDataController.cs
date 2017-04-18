dc3c178f1c2530329adbe6dd4809073c2d5121df
Steve Lasker <StevenLasker@Hotmail.com>
2017-04-18 09:42:23 -0700

    Pre Demo Changes

--- MyCompany.Visitors.CRMSvc/Controllers/CRMDataController.cs
+++ MyCompany.Visitors.CRMSvc/Controllers/CRMDataController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MyCompany.Visitors.CRMSvc.Controllers;
 
 namespace MyCompany.Visitors.CRMSvc.Controllers
 {
